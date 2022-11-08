27445cb1f45d0612ce53cc7eb2df50dbbca50e75
dtaleme <kiwi0tails@gmail.com>
2022-11-09 00:30:10 +0300

    lab4

---
+++ Lab4/translations/ru.auroraos.Lab4.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="57"/>
+        <source>Счётчик: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task01_1</name>
+    <message>
+        <location filename="../qml/pages/Task01_1.qml" line="48"/>
+        <source>Глубина стека: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task01_1.qml" line="55"/>
+        <source>Task01_%1.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task01_1.qml" line="63"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task01_2</name>
+    <message>
+        <location filename="../qml/pages/Task01_2.qml" line="48"/>
+        <source>Глубина стека: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task01_2.qml" line="55"/>
+        <source>Task01_%1.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task01_3</name>
+    <message>
+        <location filename="../qml/pages/Task01_3.qml" line="48"/>
+        <source>Глубина стека: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task02_1</name>
+    <message>
+        <location filename="../qml/pages/Task02_1.qml" line="53"/>
+        <source>Task02_2.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task02_1.qml" line="66"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task03</name>
+    <message>
+        <location filename="../qml/pages/Task03.qml" line="83"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task04</name>
+    <message>
+        <location filename="../qml/pages/Task04.qml" line="72"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task05</name>
+    <message>
+        <location filename="../qml/pages/Task05.qml" line="71"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task06</name>
+    <message>
+        <location filename="../qml/pages/Task06.qml" line="73"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task07</name>
+    <message>
+        <location filename="../qml/pages/Task07.qml" line="62"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task08</name>
+    <message>
+        <location filename="../qml/pages/Task08.qml" line="86"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task09</name>
+    <message>
+        <location filename="../qml/pages/Task09.qml" line="94"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task10</name>
+    <message>
+        <location filename="../qml/pages/Task10.qml" line="79"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
