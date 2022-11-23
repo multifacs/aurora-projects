46c6697014222a4e17efa3bb7c0a983106fdca0d
dtaleme <kiwi0tails@gmail.com>
2022-11-23 22:21:23 +0300

    lab5

---
+++ vorobyova/Lab5/translations/ru.auroraos.Lab5.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="47"/>
+        <source>Application Template</source>
+        <translation>Application Template</translation>
+    </message>
+</context>
+<context>
+    <name>Task1</name>
+    <message>
+        <location filename="../qml/pages/Task1.qml" line="48"/>
+        <source>Задание 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task1.qml" line="86"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task2</name>
+    <message>
+        <location filename="../qml/pages/Task2.qml" line="48"/>
+        <source>Задание 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task2.qml" line="113"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task3</name>
+    <message>
+        <location filename="../qml/pages/Task3.qml" line="48"/>
+        <source>Задание 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task3.qml" line="88"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task4</name>
+    <message>
+        <location filename="../qml/pages/Task4.qml" line="49"/>
+        <source>Задание 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task4.qml" line="84"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task5</name>
+    <message>
+        <location filename="../qml/pages/Task5.qml" line="49"/>
+        <source>Задание 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task5.qml" line="97"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task6</name>
+    <message>
+        <location filename="../qml/pages/Task6.qml" line="49"/>
+        <source>Задание 6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task6.qml" line="127"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task7</name>
+    <message>
+        <location filename="../qml/pages/Task7.qml" line="49"/>
+        <source>Задание 7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task7.qml" line="61"/>
+        <source>12h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task7.qml" line="61"/>
+        <source>24h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task7.qml" line="62"/>
+        <source>Switch with text label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task7.qml" line="73"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Task8</name>
+    <message>
+        <location filename="../qml/pages/Task8.qml" line="49"/>
+        <source>Задание 8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task8.qml" line="71"/>
+        <source>Active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task8.qml" line="71"/>
+        <source>Inactive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task8.qml" line="72"/>
+        <source>Switch with text label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Task8.qml" line="84"/>
+        <source>Pages.qml</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
